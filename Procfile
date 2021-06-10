a1c8a78c85bf8d2fec120a515e10e12b24dde770
ProfJAT <46096425+ProfJAT@users.noreply.github.com>
2021-06-10 15:17:18 -0700

    Heroku setup

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn algotrader.wsgi
--- algotrader/algotrader/settings.py
+++ algotrader/algotrader/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,3 +130,6 @@ STATICFILES_DIRS = [
 
 LOGIN_REDIRECT_URL = "profile"
 LOGIN_URL = "login"
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
